7599232151ffe03996d2d7b47eb10ed6c5c82aac
Ryan Gehris <rogehris@gmail.com>
2022-10-24 17:16:32 -0700

    Fixed test suite for JSONstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,37 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // I - any data type
+  // O - stringified
+  // C -
+  // E - number, boolean, and null need to be converted to string
+  //   - strings add extra set of quotations
+  //   - functions and undefined return undefined
+  //   - if key is undefined, make it a string
+  //   - if key is function, do not include in stringified object
+  //   - if object value is function or undefined then do not include
+
+  // strategy
+
+  var stringified = '';
+
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return stringified + obj; // test +=
+  }
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (typeof obj === 'function' || obj === undefined) {
+    return undefined;
+  }
+
+  // if obj is an array
+    // add opening square bracket to stringified
+    // iterate over the array elements
+      // for each element
+
+
+  return stringified;
 };
